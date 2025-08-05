cf7b7cc1441ba287defc2185916dd795f1ef9737
Damian Mendez <kiliranshe@hotmail.com>
2025-08-05 11:48:36 +0200

    Preparacion librerias test. Desacoplamos logica del monobehaviour AudioEnviroment a un evaluator y preparamos tests.

---
+++ Assets/Scripts/Core/AudioEnviromentEvaluator.cs
@@ -0,0 +1,53 @@
+﻿using Game.Data;
+using Game.Events;
+using Game.Utilities;
+using System.Collections.Generic;
+
+namespace Game.Core
+{
+    public class AudioEnviromentEvaluator
+    {
+        private readonly Dictionary<AudioEnvConfig, bool> hasPlayed = new();
+        private readonly IEventBus<SFXEvent> eventBus;
+        private readonly List<AudioEnvConfig> currentDayAudioConfigs;
+        private float elapsedMinutes;
+
+        public AudioEnviromentEvaluator(List<AudioEnvConfig> configs, IEventBus<SFXEvent> eventBus)
+        {
+            this.currentDayAudioConfigs = configs;
+            this.eventBus = eventBus;
+
+            foreach (var config in configs)
+                hasPlayed[config] = false;
+        }
+
+        public void UpdateTime(float deltaTime)
+        {
+            elapsedMinutes += deltaTime / 60f;
+            EvaluateConditions();
+        }
+
+        private void EvaluateConditions()
+        {
+            foreach (var audioConfig in currentDayAudioConfigs)
+            {
+                if (hasPlayed[audioConfig]) continue;
+
+                if (ShouldPlayAudio(audioConfig))
+                {
+                    eventBus.Raise(new SFXEvent { sfxData = audioConfig.AudioEmitterData });
+                    hasPlayed[audioConfig] = true;
+                }
+            }
+        }
+
+        private bool ShouldPlayAudio(AudioEnvConfig config)
+        {
+            return config.triggerCondition switch
+            {
+                AudioTriggerCondition.TimePassedMinutes => elapsedMinutes >= config.triggerValue,
+                _ => false,
+            };
+        }
+    }
+}
--- Assets/Scripts/Core/AudioEnviromentSystem.cs
+++ Assets/Scripts/Core/AudioEnviromentSystem.cs
@@ -2,76 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Game.Events;
-using Game.Utilities;
 using Game.Data;
 
 namespace Game.Core
 {
     public class AudioEnviromentSystem : MonoBehaviour
     {
-        private List<AudioEnvConfig> currentDayAudioConfigs;
-        private Dictionary<AudioEnvConfig, bool> hasPlayed = new();
-        private IEventBus<SFXEvent> eventBus;
-
-        private float elapsedTime = 0f;
-        private int fishFedCount = 0;
+        private AudioEnviromentEvaluator evaluator;
 
         public void Initialize(List<AudioEnvConfig> audiosCurrentDay, int daysPassed, IEventBus<SFXEvent> sfxEventBus)
         {
-            this.eventBus = sfxEventBus;
-
-            currentDayAudioConfigs = audiosCurrentDay;
-            hasPlayed.Clear();
-
-            foreach (var c in currentDayAudioConfigs)
-                hasPlayed[c] = false;
+            evaluator = new AudioEnviromentEvaluator(audiosCurrentDay, sfxEventBus);
 
             StartCoroutine(CheckAudioConditionsCoroutine(60));
         }
 
-        public void Update()
-        {
-            elapsedTime += Time.deltaTime;
-        }
         private IEnumerator CheckAudioConditionsCoroutine(float interval)
         {
             var wait = new WaitForSeconds(interval);
 
             while (true)
             {
-                EvaluateConditions();
+                evaluator.UpdateTime(interval);
                 yield return wait;
             }
         }
 
-        private void EvaluateConditions()
-        {
-           
-
-            foreach (var audioConfig in currentDayAudioConfigs)
-            {
-                if (hasPlayed[audioConfig]) continue;
-
-                if (ShouldPlayAudio(audioConfig))
-                {
-                    eventBus.Raise(new SFXEvent { sfxData = audioConfig.AudioEmitterData});
-                    hasPlayed[audioConfig] = true;
-                }
-            }
-        }
-        private bool ShouldPlayAudio(AudioEnvConfig config)
-        {
-            switch (config.triggerCondition)
-            {
-                case AudioTriggerCondition.TimePassedMinutes:
-                    return elapsedTime / 60f >= config.triggerValue;
-
-                case AudioTriggerCondition.FishFedTimes:
-                    return fishFedCount >= config.triggerValue;
-
-                default:
-                    return false;
-            }
-        }
+      
     }
 }
